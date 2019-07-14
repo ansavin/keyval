bfe6409ab36452e85f951b1063e653a966a4cbc3
ansavin <Andrei.V.Savin@ya.ru>
2019-07-14 20:25:44 +0300

    add simple input from cli

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+    "fmt"
+	"os"
+	"strings"
 )
 
 var storage map[string]string
@@ -12,9 +15,30 @@ var storage map[string]string
 	del() string
 }
 */
+func upload_to_storage(data []string) {
+	for index := 0; index < len(data); index++ {
+		key := index
+		//fmt.Println(index)
+		if index++; index >= len(data) {
+			//fmt.Println("foo")
+			//fmt.Println(index)
+			storage[data[key]] = ""
+		} else {
+			//fmt.Println("bar")
+			//fmt.Println(index)
+			storage[data[key]] = data[index]
+		}
+	}
+}
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	storage = make(map[string]string)
-	storage["1"] = "data"
-	fmt.Println(storage["1"])
+	input, _ := reader.ReadString('\n')
+	data := strings.Fields(input)
+	//fmt.Println("len", len(data))
+	upload_to_storage(data)
+	for key, value := range storage {
+		fmt.Printf("%v: %v\n", key, value)
+	}
 }
